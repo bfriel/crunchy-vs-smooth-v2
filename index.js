6cbcc31ae5a2e73fb6c51893e85abae12888b0c4
Brian Friel <briandfriel@gmail.com>
2021-10-11 22:39:11 -0700

    Remove old call for voteAccount

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const path = require("path");
-const anchor = require("@project-serum/anchor");
 const express = require("express");
-
-const voteAccount = anchor.web3.Keypair.generate();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "app/build")));
-
-app.get("/voteAccount", (req, res) => {
-  res.json({ voteAccount });
-});
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "app/build", "index.html"));
